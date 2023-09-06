4664f7c4a039c96d4b73ce77dab4efbbcf7eaf37
Moon Baboon <hungry.snails.inc@gmail.com>
2023-09-06 14:48:07 +0300

    Homework: Page Objects

---
+++ src/main/java/org/prog/homework/pages/AbstractPage.java
@@ -0,0 +1,20 @@
+package org.prog.homework.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class AbstractPage {
+
+    protected final WebDriver driver;
+    private final String url;
+
+    public AbstractPage(WebDriver driver, String url) {
+        this.driver = driver;
+        this.url = url;
+    }
+
+    public void loadPage() {
+        driver.get("about::blank");
+        driver.get(url);
+    }
+
+}
